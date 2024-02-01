5aab40e6e6f0e6d39cfc91f833dec486a2ef8211
ipodprugin <p0dprugin.ilia@gmail.com>
2024-02-01 16:36:26 +0300

    updated config

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,9 +1,25 @@
 return {
-  -- Add the community repository of plugin specifications
   "AstroNvim/astrocommunity",
-  -- example of importing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  {
+    "catppuccin",
+    opts = {
+      integrations = {
+        rainbow_delimiters = true,
+      },
+      -- transparent_background = true,
+    },
+  },
 
-  -- { import = "astrocommunity.colorscheme.catppuccin" },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+  { import = "astrocommunity.colorscheme.dracula-nvim" },
+
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+
+  { import = "astrocommunity.completion.codeium-vim" },
+
+  { import = "astrocommunity.remote-development.netman-nvim" },
+
+  { import = "astrocommunity.split-and-window.windows-nvim" },
+
+  -- { import = "astrocommunity.media.pets-nvim" },
 }
